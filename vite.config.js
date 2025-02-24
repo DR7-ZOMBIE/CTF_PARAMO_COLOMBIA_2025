9740bf38ce5b099886add76f98ff4745acfe783e
DR7-ZOMBIE <dcr7juilland@gmail.com>
2025-02-23 21:57:05 -0500

    Fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  base: "/CTF_PARAMO_COLOMBIA_2025/", // Asegúrate de que coincide con el nombre del repo
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  }
+});
